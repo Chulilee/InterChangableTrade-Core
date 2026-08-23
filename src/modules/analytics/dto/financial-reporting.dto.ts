ad0f66c4ae06f1d5bee6390abb3833294d4d0f1f
DevMuhdishaq <muhmdishaq001@gmail.com>
2026-08-23 07:50:55 +0100

    feat(analytics): add financial reporting

---
+++ src/modules/analytics/dto/financial-reporting.dto.ts
@@ -0,0 +1,43 @@
+import { IsOptional, IsDateString, IsEnum, IsInt, Min, Max } from 'class-validator';
+import { MetricType } from '../entities/analytics-metric.entity';
+
+export class GetRevenueBreakdownDto {
+  @IsDateString()
+  dateFrom: string;
+
+  @IsDateString()
+  dateTo: string;
+}
+
+export class GetCostAnalysisDto {
+  @IsDateString()
+  dateFrom: string;
+
+  @IsDateString()
+  dateTo: string;
+}
+
+export class GetProfitabilityMetricsDto {
+  @IsDateString()
+  dateFrom: string;
+
+  @IsDateString()
+  dateTo: string;
+}
+
+export class GetForecastDataDto {
+  @IsEnum(MetricType)
+  metricType: MetricType;
+
+  @IsOptional()
+  @IsInt()
+  @Min(3)
+  @Max(60)
+  historicalMonths?: number = 12;
+
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(24)
+  forecastMonths?: number = 6;
+}
